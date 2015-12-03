c09228edadb303923326546dada6d251a29b2fce
iwanswidodo <iwanf2509@gmail.com>
2015-12-03 16:09:06 +0700

    ini tugas saya iwan

---
+++ MI2 D_1531143004/1531143004_Iwan_Suryo_Widodo_2DMI/1531143004_Iwan_Suryo_Widodo_2DMI/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.BackColor = Color.Chocolate
+
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        If Me.BackColor = Color.Chocolate Then
+            Me.BackColor = Color.Blue
+        Else
+            Me.BackColor = Color.Chocolate
+
+
+        End If
+    End Sub
+End Class
